14b1ffac47e014d1234701c9d83ab060c4fcd3c0
DraWar287 <Dragon_Hero287@outlook.com>
2024-12-08 21:04:12 +0800

    Add target directory to .gitignore

--- target/debug/deps/simple_tcp_ip-fd9ab3950b010150.d
+++
@@ -1,7 +0,0 @@
-/home/drawar/my/opensource/simple_tcp_ip_stack/simple-TCP-IP-protocol-stack/simple_tcp_ip/target/debug/deps/simple_tcp_ip-fd9ab3950b010150: src/main.rs src/net/mod.rs src/net/ipv4.rs
-
-/home/drawar/my/opensource/simple_tcp_ip_stack/simple-TCP-IP-protocol-stack/simple_tcp_ip/target/debug/deps/simple_tcp_ip-fd9ab3950b010150.d: src/main.rs src/net/mod.rs src/net/ipv4.rs
-
-src/main.rs:
-src/net/mod.rs:
-src/net/ipv4.rs:
